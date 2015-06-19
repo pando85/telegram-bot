9aef3f2282c8a7c667a25aeef3e4a5adf050cc16
server <server@telegram-bot.defaultdomain>
2015-06-19 13:25:15 +0000

    Revert "arreglado chname!"
    This reverts commit 9f748af75131cb3169004a86850c1eee4e0a4b1d.

--- plugins/chname.lua
+++ plugins/chname.lua
@@ -1,15 +1,12 @@
 
 local function run(msg, matches)
-  local text = string.sub(matches[1], 1, 50)
+  local text = matches[1]
 
   if not is_chat_msg(msg) then
     return "You are not in a group chat"
   end
 
---  rename_chat(get_receiver(msg), text)
-  local receiver = get_receiver(msg)
-  print('changing name of chat: '..receiver..' to '..text)
-  rename_chat(receiver, text, cb_ok, false)
+  rename_chat(get_receiver(msg), text)
   return "Done!"
 end
 
