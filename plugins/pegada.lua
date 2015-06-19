2302d8e2739e9199658a77790b3295bef647557a
server <server@telegram-bot.defaultdomain>
2015-06-19 13:29:17 +0000

    Revert "Added history to pegada"
    This reverts commit 9006283e7b525900f7fb39ea6459826d14656b88.

--- plugins/pegada.lua
+++ plugins/pegada.lua
@@ -1,29 +1,17 @@
 local math = require('math')
-local history = {}
 
 local function run(msg, matches)
-  value = math.random(1, 6)
+  value = math.random(1, 20)
 
   name = msg.from.first_name
   if name == "" then
     name = "pedazo de mierda"
   end
 
-  if history[name] == nil then
-    history[name] = 0
+  if value == 1 then
+    print ('Pegándosela a '..name..'.')
+    return 'Cállate, '..name..', por favor.'
   end
-
-  history[name] += 1
-
-  if history[name] >= 10 then
-    if value == 1 then
-      print ('Pegándosela a '..name..'.')
-      history[name] = 0
-      return 'Cállate, '..name..', por favor.'
-    end
-  end
-
-
 end
 
 return {
