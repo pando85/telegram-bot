faf398fded47d689036b4621cef4664d9e96ff1d
pando85 <pando855@gmail.com>
2015-06-19 15:51:53 +0200

    Sodexoooooooooo

--- plugins/pegada.lua
+++ plugins/pegada.lua
@@ -1,8 +1,4 @@
 local math = require('math')
-<<<<<<< HEAD
-=======
-local history = {}
->>>>>>> parent of e1897dd... Fixed history var init problem
 
 local function run(msg, matches)
   value = math.random(1, 20)
