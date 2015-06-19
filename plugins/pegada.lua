ea8cac68f26f1b56b42561f04c60780de2f306cf
server <server@telegram-bot.defaultdomain>
2015-06-19 14:08:44 +0000

    La pegada es ahora configurable usando set

--- plugins/pegada.lua
+++ plugins/pegada.lua
@@ -1,7 +1,35 @@
 local math = require('math')
+local var_name = 'pegada_max_prob'
+local default_max_prob = 20
+
+local function get_variables_hash(msg)
+  if msg.to.type == 'chat' then
+    return 'chat:'..msg.to.id..':variables'
+  end
+  if msg.to.type == 'user' then
+    return 'user:'..msg.from.id..':variables'
+  end
+end
+
+local function get_int_value(redis_value)
+  if not redis_value then
+    return default_max_prob
+  end
+
+  local int_value = tonumber(redis_value)
+  if not int_value or int_value < 1 then
+    return default_max_prob
+  end
+
+  return int_value
+end
 
 local function run(msg, matches)
-  value = math.random(1, 20)
+  local hash = get_variables_hash(msg)
+  local max_prob = get_int_value(redis:hget(hash, var_name))
+
+  print ('max_prob: '..max_prob)
+  value = math.random(1, max_prob)
 
   name = msg.from.first_name
   if name == "" then
@@ -16,7 +44,7 @@ end
 
 return {
   description = "El plugin de Dios de la Peñita",
-  usage = "Josu se jode",
+  usage = "La pega de vez en cuando. Usa la variable 'pegada_max_prob' para configurar la frecuencia.",
   patterns = {
     "..*"
   }, 
