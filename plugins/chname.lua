4cb17fe23bed236b8b311fbb2c6800888d057744
server <server@telegram-bot.defaultdomain>
2015-06-19 13:38:34 +0000

    Fixed chname

--- plugins/chname.lua
+++ plugins/chname.lua
@@ -1,20 +1,23 @@
 
 local function run(msg, matches)
-  local text = matches[1]
+local text = string.sub(matches[1], 1, 50)
 
-  if not is_chat_msg(msg) then
-    return "You are not in a group chat"
-  end
+if not is_chat_msg(msg) then
+   return "You are not in a group chat"
+end
 
-  rename_chat(get_receiver(msg), text)
-  return "Done!"
+-- rename_chat(get_receiver(msg), text)
+local receiver = get_receiver(msg)
+   print('changing name of chat: '..receiver..' to '..text)
+   rename_chat(receiver, text, cb_ok, false)
+   return "Done!"
 end
 
 return {
-  description = "Changes the group name",
-  usage = "!chname [group name]: sets the group name to [group name]",
-  patterns = {
-    "^!chname +(.+)$"
-  }, 
-  run = run 
+   description = "Changes the group name",
+   usage = "!chname [group name]: sets the group name to [group name]",
+   patterns = {
+              "^!chname +(.+)$"
+              },
+              run = run
 }
