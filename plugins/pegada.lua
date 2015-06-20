b94210540a360cecc94a804d1be64961aae090ab
Pando85 <pando855@gmail.com>
2015-06-20 12:08:30 +0200

    Pegada now is more effective in low range pegada

--- plugins/pegada.lua
+++ plugins/pegada.lua
@@ -1,6 +1,7 @@
 local math = require('math')
-local var_name = 'pegada_max_prob'
-local default_max_prob = 20
+local counter_start = 0
+local max_prob = 4
+local counter_flag = 5
 
 local function get_variables_hash(msg)
   if msg.to.type == 'chat' then
@@ -13,12 +14,13 @@ end
 
 local function get_int_value(redis_value)
   if not redis_value then
-    return default_max_prob
+      redis:hset(hash, name, counter_start)
+    return counter_start
   end
 
-  local int_value = tonumber(redis_value)
+  local int_value = redis_value
   if not int_value or int_value < 1 then
-    return default_max_prob
+    return counter_start
   end
 
   return int_value
@@ -26,15 +28,22 @@ end
 
 local function run(msg, matches)
   local hash = get_variables_hash(msg)
-  local max_prob = get_int_value(redis:hget(hash, var_name))
-
-  print ('max_prob: '..max_prob)
-  value = math.random(1, max_prob)
 
   name = msg.from.first_name
   if name == "" then
     name = "pedazo de mierda"
   end
+
+  local counter = get_int_value(redis:hget(hash, name))
+
+  value = math.random(1, max_prob)
+  print ('max_prob: '..max_prob)  
+
+  if counter < counter_flag then
+    value = 0
+  end
+
+  redis:hset(hash, name, counter + 1)
 
   if value == 1 then
     print ('Pegándosela a '..name..'.')
