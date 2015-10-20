80994af083db55a00ccef63efee71272b0cbea99
Pando85 <pando855@gmail.com>
2015-10-20 19:15:03 +0200

    Finished?

--- plugins/insultar.lua
+++ plugins/insultar.lua
@@ -1,6 +1,6 @@
 local math = require('math')
 local counter_start = 0
-local max_prob = 2
+local max_prob = 4
 local counter_flag = 1
 
 local insultos = {"abanto","abrazafarolas","adufe","alcornoque","alfeñique","andurriasmo","arrastracueros","artabán","atarre","baboso","barrabás","barriobajero","bebecharcos","bellaco","belloto","berzotas","besugo","bobalicón","bocabuzón","bocachancla","bocallanta","boquimuelle","borrico","botarate","brasas","cabestro","cabezaalberca","cabezabuque","cachibache","cafre","cagalindes","cagarruta","calambuco","calamidad","caldúo","calientahielos","calzamonas","cansalmas","cantamañanas","capullo","caracaballo","caracartón","caraculo","caraflema","carajaula","carajote","carapapa","carapijo","cazurro","cebollino","cenizo","cenutrio","ceporro","cernícalo","charrán","chiquilicuatre","chirimbaina","chupacables","chupasangre","chupóptero","cierrabares","cipote","comebolsas","comechapas","comeflores","comestacas","cretino","cuerpoescombro","culopollo","descerebrado","desgarracalzas","dondiego","donnadie","echacantos","ejarramantas","energúmeno","esbaratabailes","escolimoso","escornacabras","estulto","fanfosquero","fantoche","fariseo","filimincias","foligoso","fulastre","ganapán","ganapio","gandúl","gañán","gaznápiro","gilipuertas","giraesquinas","gorrino","gorrumino","guitarro","gurriato","habahelá","huelegatera","huevón","lamecharcos","lameculos","lameplatos","lechuguino","lerdo","letrín","lloramigas","longanizas","lumbreras","maganto","majadero","malasangre","malasombra","malparido","mameluco","mamporrero","manegueta","mangarrán","mangurrián","mastuerzo","matacandiles","meapilas","melón","mendrugo","mentecato","mequetrefe","merluzo","metemuertos","metijaco","mindundi","morlaco","morroestufa","muerdesartenes","orate","ovejo","pagafantas","palurdo","pamplinas","panarra","panoli","papafrita","papanatas","papirote","paquete","pardillo","parguela","pasmarote","pasmasuegras","pataliebre","patán","pavitonto","pazguato","pecholata","pedorro","peinabombillas","peinaovejas","pelagallos","pelagambas","pelagatos","pelatigres","pelazarzas","pelele","pelma","percebe","perrocostra","perroflauta","peterete","petimetre","picapleitos","pichabrava","pillavispas","piltrafa","pinchauvas","pintamonas","piojoso","pitañoso","pitofloro","plomo","pocasluces","pollopera","quitahipos","rastrapajo","rebañasandías","revientabaules","ríeleches","robaperas","ronchalastimas","sabandija","sacamuelas","sanguijuela","sinentraero","sinsustancia","sonajas","sonso","soplagaitas","soplaguindas","sosco","tagarote","tarado","tarugo","tiralevitas","tocapelotas","tocho","tolai","tontaco","tontucio","tordo","tragaldabas","tuercebotas","tunante","zamacuco","zambombo","zampabollos","zamugo","zángano","zarrapastroso","zascandil","zopenco","zoquete","zote","zullenco","zurcefrenillos"}
@@ -12,14 +12,6 @@ local function Set (list)
 end
 
 local targets = Set {33698741}
-
-local function calculate_size(array)
-  local count = 0
-  for Index, Value in pairs( array ) do
-    count = count + 1
-  end
-  return count
-end
 
 local function get_variables_hash(msg)
   if msg.to.type == 'chat' then
@@ -67,9 +59,8 @@ local function run(msg, matches)
 
   if (value == 1 and targets[user_id]) then
     print ('Insultando a '..name..'.')
-    value = math.random(1, calculate_size(insultos))
     redis:hset(hash, name, counter_start)
-    return 'Que pasa '..name..' pareces el típico '..insultos[value]'.'
+    return 'Que pasa '..name..' pareces el típico '..insultos[math.random(#insultos)]..'.'
   end
 end
 
