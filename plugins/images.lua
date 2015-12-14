f5949523c7a079281052f8b035b142b5b307d3e5
Pando85 <pando855@gmail.com>
2015-12-14 12:50:10 +0100

    Fixes google img (reverted from commit 43fc61a50c3b2db473784d966185a2f1cd82059e)

--- plugins/images.lua
+++ plugins/images.lua
@@ -1,34 +1,20 @@
+do
 
 function run(msg, matches)
-  local input = matches[1]
+  local url = matches[1]
   local receiver = get_receiver(msg)
-
-  local url = 'https://www.googleapis.com/customsearch/v1?'
-  url = url .. 'key=0000' -- KEY Get https://console.developers.google.com/apis/credentials
-  url = url .. '&cx=00000:abcd' -- CX Get https://cse.google.com/cse
-  url = url .. '&searchType=image&imgSize=xlarge&alt=json&num=5&start=1'
-  url = url .. '&q=' .. (URL.escape(input) or "")
-
-  local res, rest = https.request(url)
-  if rest ~=200 then return "ERRO URL" end
-
-  local jdat = json:decode(res)
-  local numb = jdat.queries.request[1].count or '1'
-  local random = math.random(1, numb)
-  local url_img = jdat.items[numb].link
-
-  send_photo_from_url(receiver, url_img)
-  -- Send MSG
-  return nil
+  send_photo_from_url(receiver, url)
 end
 
---Run
 return {
-  description = "Img google",
-  usage = "/img",
+  description = "When user sends image URL (ends with png, jpg, jpeg) download and send it to origin.", 
+  usage = "",
   patterns = {
-    "^/[Ii]mg$",
-    "^/[Ii]mg (.*)$",
-  },
-  run = run
+    "(https?://[%w-_%.%?%.:/%+=&]+%.png)$",
+    "(https?://[%w-_%.%?%.:/%+=&]+%.jpg)$",
+    "(https?://[%w-_%.%?%.:/%+=&]+%.jpeg)$",
+  }, 
+  run = run 
 }
+
+end
